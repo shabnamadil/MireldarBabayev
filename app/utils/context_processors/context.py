78009d46b222756f7a269c625e55197ee5253abd
shabnamadil <sebnemadil1999@gmail.com>
2024-11-18 18:07:45 +0400

    some updates added

--- app/utils/context_processors/context.py
+++ app/utils/context_processors/context.py
@@ -5,11 +5,14 @@ from apps.core.models import (
     StatisticalIndicator
 )
 
+from apps.service.models import Service
+
 def context_info(request):
     context={
         'current_year': datetime.datetime.now().year,
         'current_time': datetime.datetime.now(),
         'settings': SiteSettings.objects.first(),
-        'indicators' : StatisticalIndicator.objects.all()[:3]
+        'indicators' : StatisticalIndicator.objects.all()[:3],
+        'footer_services' : Service.objects.all()
     }
     return context
