a747ca8f266cf0efc909be7e4fd72edec53d7319
shabnamadil <sebnemadil1999@gmail.com>
2025-02-06 15:29:46 +0400

    everything up to date unit core model tests

---
+++ app/utils/helpers/validate_social_media.py
@@ -0,0 +1,34 @@
+from django.core.exceptions import ValidationError
+
+import re
+
+
+def validate_facebook(value):
+    pattern = r'^https?://(www\.)?facebook\.com/.*$'
+    if not re.match(pattern, value):
+        raise ValidationError("Invalid Facebook URL. Please enter a valid Facebook link.")
+
+def validate_youtube(value):
+    pattern = r'^https?://(www\.)?youtube\.com/.*$'
+    if not re.match(pattern, value):
+        raise ValidationError("Invalid YouTube URL. Please enter a valid YouTube link.")
+
+def validate_twitter(value):
+    pattern = r'^https?://(www\.)?twitter\.com/.*$'
+    if not re.match(pattern, value):
+        raise ValidationError("Invalid Twitter URL. Please enter a valid Twitter link.")
+
+def validate_instagram(value):
+    pattern = r'^https?://(www\.)?instagram\.com/.*$'
+    if not re.match(pattern, value):
+        raise ValidationError("Invalid Instagram URL. Please enter a valid Instagram link.")
+
+def validate_linkedin(value):
+    pattern = r'^https?://(www\.)?linkedin\.com/.*$'
+    if not re.match(pattern, value):
+        raise ValidationError("Invalid LinkedIn URL. Please enter a valid LinkedIn link.")
+
+def validate_tiktok(value):
+    pattern = r'^https?://(www\.)?tiktok\.com/.*$'
+    if not re.match(pattern, value):
+        raise ValidationError("Invalid TikTok URL. Please enter a valid TikTok link.")
